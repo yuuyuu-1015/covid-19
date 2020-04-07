0fb2860f9bd4af666459a9c85ad0874c38fa2006
Yuta Fukuchi <fufufu10155@gmail.com>
2020-04-07 20:13:37 +0900

    script.js 作成

--- src/data/script.js
+++ src/data/script.js
@@ -19,7 +19,11 @@ const main = async () => {
 const csvToJson = (csv) => {
   // 変換処理
   // 何かオブジェクトなり配列なりを返す
-  csv
+  const result = []
+  for (let i = 0, l = csv.length; i < l; i++) {
+    result.push({ date: csv[i][0], location: csv[i][1], new_cases: csv[i][2], new_deaths: csv[i][3], total_cases: csv[i][4], total_deaths: csv[i][5]})
+  }
+  return result
 }
 
 main()
